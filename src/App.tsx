5ee489709c065930f1208b4fd156c810638e802a
Giuseppe Maggiore <giuseppemaggiore@Giuseppes-MacBook-Pro.local>
2020-03-30 09:44:29 +0200

    Initial changes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+import { Widget, stateful, Action, any, mkWidget } from "widgets-for-react"
+import { setter } from "ts-lenses"
+import { TextField } from 'office-ui-fabric-react/lib/TextField'
+import { Stack } from 'office-ui-fabric-react/lib/Stack'
+
+const JSXWidget = function<s>(w:(setState:(_:s) => void) => JSX.Element) : Widget<s> {
+  return mkWidget({run:w})
+}
+
+interface State {
+  name:string
+  surname:string
+  age:number
+}
+
+const app = () : Widget<State> => 
+  stateful<State>()(s0 => 
+    any<Action<State>>()([
+      JSXWidget(
+        setState =>
+          <TextField label="Name" value={s0.name} onChange={e => setState(setter("name", (e.target as any).value))} />
+      ),
+      JSXWidget(
+        setState =>
+          <TextField label="Surname" value={s0.surname} onChange={e => setState(setter("surname", (e.target as any).value))} />
+      ),
+      JSXWidget(
+        setState =>
+          <TextField label="Age" type={"number"} value={s0.age as any} onChange={e => setState(setter("age", (e.target as any).valueAsNumber))} />
+        )
+    ]).map(a => a(s0)).wrapHTML(_ => <Stack>{_}</Stack>)
+  )({ name:"", surname:"", age:18 })
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {app().run(s => console.log(`The state is now: ${JSON.stringify(s)}`))}
     </div>
   );
 }
